03de0fe9d7b86ad1fff166f45eaf2892c522c5c0
Nicholas Pfleegor <ndp5030@gmail.com>
2022-05-05 00:55:42 -0400

    Finished chapter 3 studio

---
+++ ChThreeStudio/CharCount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChThreeStudio
+{
+    internal class CharCount
+    {
+        public static Dictionary<char, int> myCount()
+        {
+            Dictionary<char, int> output = new Dictionary<char, int>();
+            string input;
+            Console.WriteLine("Please enter a string: ");
+            input = Console.ReadLine();
+
+            foreach (char character in input)
+            {
+                if (!output.ContainsKey(character))
+                {
+                    output.Add(character, 1);
+                } else
+                {
+                    output[character]++;
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ ChThreeStudio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChThreeStudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            // used for first part of studio
+            // Dictionary <char, int> countedChars = CharCount.myCount(myString);
+            // used for bonuses
+            Dictionary<char, int> countedChars = CharCount.myCount();
+            Console.WriteLine(countedChars);
+
+            foreach (KeyValuePair<char, int> kvp in countedChars)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+            
+
+        }
+    }
+}
